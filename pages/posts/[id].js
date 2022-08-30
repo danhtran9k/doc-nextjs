d24723035bb687885d6b85d03519c4401fb9fd97
Tran Luong Danh <danh2k@nexondv.com>
2022-08-30 11:51:27 +0700

    render markdown comment

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Layout from "../../components/layout";
 
 import { getAllPostIds, getPostData } from "../../lib/posts";
@@ -11,7 +12,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-	const postData = getPostData(params.id);
+	const postData = await getPostData(params.id);
 	return {
 		props: {
 			postData,
@@ -27,6 +28,8 @@ export default function Post({ postData }) {
 			{postData.id}
 			<br />
 			{postData.date}
+			<br />
+			<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
 		</Layout>
 	);
 }
